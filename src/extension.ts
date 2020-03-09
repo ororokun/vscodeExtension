a10c0d86aa236a62a98a6203ead697e004d4a417
Fukawa <desertodeitartari42@gmail.com>
2020-03-09 17:34:59 +0900

    猫を表示する

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,10 @@ import * as vscode from "vscode";
 let count: number = 0;
 
 export function activate(context: vscode.ExtensionContext) {
+  vscode.env.openExternal(vscode.Uri.parse("https://example.com"));
   setTimeout(countTime, 1000);
   let activeEditor = vscode.window.activeTextEditor;
-  vscode.workspace.onDidChangeTextDocument(event => {
+  vscode.workspace.onDidChangeTextDocument(() => {
     if (count >= 60) {
       count = 0;
       setTimeout(countTime, 1000);
@@ -12,6 +13,20 @@ export function activate(context: vscode.ExtensionContext) {
       count = 0;
     }
   });
+}
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
 }
 
 let countTime = (): void => {
@@ -25,7 +40,13 @@ let countTime = (): void => {
 };
 
 let callMenor = (): void => {
-  console.log("mentor!!!!");
+  const panel = vscode.window.createWebviewPanel(
+    "catCoding", // Identifies the type of the webview. Used internally
+    "Cat Coding", // Title of the panel displayed to the user
+    vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+    {} // Webview options. More on these later.
+  );
+  panel.webview.html = getWebviewContent();
 };
 
 // this method is called when your extension is deactivated
